d70b93a42b49e6dbd0b020be956e0477c409e7f9
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2021-11-28 23:33:12 +0300

    bubbleSorting added

--- src/main/java/ArraysAdvancedDemo.java
+++ src/main/java/ArraysAdvancedDemo.java
@@ -1,8 +1,10 @@
 public class ArraysAdvancedDemo {
     public static void main(String[] args) {
-        int[] arr = new int[]{-10,-9,-8,-7,-6,-5,-3,-1,0,8};
-        int res = binarySearch(arr, 0, arr.length, -2);
-        System.out.println(res);
+       // int[] arr = new int[]{-10,-9,-8,-7,-6,-5,-3,-1,0,8};
+        //int res = binarySearch(arr, 0, arr.length, -2);
+        //System.out.println(res);
+        int[] arr2 = new int[]{2,1};
+        swapBubble(arr2);
     }
 
     static int binarySearch(int[] arr, int start, int end, int x) {
@@ -21,5 +23,25 @@ public class ArraysAdvancedDemo {
     }
 
     // Сортировка вставкой или пузырьком . Отсортирвать одномерный массив с помощью этих двух видов сортировок.
-    // +1 задача по запросу 
+    // +1 задача по запросу
+
+                                    /* Bubble Sorting */
+    static void swapValuesForSorting(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    static int[] swapBubble (int[] arr) {
+        boolean isSorted = false;  // for the first cycle
+        while(!isSorted){             // ? why cannot I put here "isSorted = false"?
+            isSorted = true;   // TRUE when the IF-statement does not occur
+            for(int i = 0; i<arr.length-1; i++) {   //arr.length-1 because of arr[i+1]
+                if(arr[i] > arr[i+1]) {
+                    swapValuesForSorting(arr, i, i+1);
+                    isSorted = false;   //one sorting has just happened, so the array must be looked through again
+                }
+            }
+        }
+        return arr;
+    }
 }
