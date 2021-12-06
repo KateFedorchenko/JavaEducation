e6e264ac53e209ab13ed2982a33a6d143c72e21b
Eduard Rakhmankulov <erixonich@gmail.com>
2021-12-06 20:54:55 +0300

    Triangle implementation problem

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,39 @@
+/**
+ * Geometric triangle.
+ */
+public class Triangle {
+    // implement methods and suitable constructor
+    // add necessary fields.
+
+    /**
+     * Area of triangle.
+     *
+     * @return Area of triangle.
+     */
+    public double area() {
+        return 0;
+    }
+
+    /**
+     * Perimeter of triangle.
+     *
+     * @return Perimeter of triangle.
+     */
+    public double perimeter() {
+        return 0;
+    }
+
+    /**
+     * True if triangle equilateral.
+     */
+    public boolean isEquilateral() {
+        return false;
+    }
+
+    /**
+     * True if triangle is right.
+     */
+    public boolean isRight() {
+        return false;
+    }
+}
