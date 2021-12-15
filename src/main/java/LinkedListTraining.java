e509ec348ab1e9d380295dc0458c2bb5b0750ae7
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2021-12-16 00:01:19 +0300

    LinkedListTraining task done

--- src/main/java/LinkedListTraining.java
+++ src/main/java/LinkedListTraining.java
@@ -9,6 +9,10 @@ public class LinkedListTraining {
             this.next = next;
         }
 
+        public Node(){
+
+        }
+
         @Override public String toString() {
             Node cur = this;
             StringBuilder sb  = new StringBuilder();
@@ -23,11 +27,15 @@ public class LinkedListTraining {
     }
 
     public static void main(String[] args) {
-        //test your code here
+        // Example of concatenation
+        Node headList = new Node("first",null);
+        Node tailList = new Node("second",null);
+        splice(headList,tailList);                                 // no use as "headList.splice(headList,tailList)" - why?
+        System.out.println(headList); // Node can be printed!
 
-        Node n = new Node("foo", new Node("bar", null));
+        // Example of LinkedList creation
+        createList("foo", "qwe", "bar", "test");
 
-        System.out.println(n); // Node can be printed!
     }
 
     /**
@@ -37,10 +45,28 @@ public class LinkedListTraining {
      * @param values Vararg of values. It is essentially String[] in the terms of usage in function.
      * @return Reference to the head node of newly created linked list
      */
-    public static Node createList(String... values) {
-
-        return new Node("foo", null);
+    public static Node createList(String... values) {                           // a bit strange - did not know this --> "String... values"
+        Node head = new Node();
+        Node node2 = new Node();
+        Node node3 = new Node();
+        Node tail = new Node();
+        for(int i = 0; i<values.length; i++) {
+             if(i == 0) {
+                head.val = values[i];
+            } else if(i == 1) {
+                node2.val = values[i];
+            } else if(i == 2) {
+                node3.val = values[i];
+            } else if(i == 3) {
+                tail.val = values[i];
+            }
+        }
+        head.next = node2;
+        node2.next = node3;
+        node3.next = tail;
+        return head;
     }
+
 
     /**
      * Concatenates two linked list with each other.
@@ -49,6 +75,7 @@ public class LinkedListTraining {
      * @param tailList Tail part of the resulting list.
      */
     public static void splice(Node headList, Node tailList) {
-
+        headList.next = tailList;
+        tailList= null;                      // seems like not ok
     }
 }
