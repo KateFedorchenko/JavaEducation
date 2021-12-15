7c503fbf8888f3f5b99782d0b1d31758ffa9fbf2
Eduard Rakhmankulov <erixonich@gmail.com>
2021-12-15 15:36:41 +0300

    Linked list training problems

---
+++ src/main/java/LinkedListTraining.java
@@ -0,0 +1,38 @@
+public class LinkedListTraining {
+
+    private static class Node {
+        String val;
+        Node next;
+
+        public Node(String val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        //test your code here
+    }
+
+    /**
+     * Creates linked list from given values.
+     * Example:
+     * {@code createList("foo", "qwe", "bar", "test") yields <foo> -> <qwe> -> <bar> -> <test> -> null}
+     * @param values Vararg of values. It is essentially String[] in the terms of usage in function.
+     * @return Reference to the head node of newly created linked list
+     */
+    public static Node createList(String... values) {
+
+        return new Node("foo", null);
+    }
+
+    /**
+     * Concatenates two linked list with each other.
+     *
+     * @param headList Head part of the resulting list.
+     * @param tailList Tail part of the resulting list.
+     */
+    public static void splice(Node headList, Node tailList) {
+
+    }
+}
