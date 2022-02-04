3ea29fcb9689a3c5af85dde4882b22780ba2fe4e
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-04 13:31:49 +0300

    Exception HW3 - Random number task

---
+++ src/main/java/ExceptionsHW3.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExceptionsHW3 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randomNumber = r.nextInt(100);
+
+        Scanner scanner = new Scanner(System.in);
+        int numOfTries = 10;
+        for (int i = 0; i < numOfTries; i++) {
+            int num = scanner.nextInt();
+            if(num == randomNumber) {
+                System.out.println("You won!");
+                return;
+            }
+            if(num > 99 || num < 0) {
+                throw new BusinessException("Your number is out of possible range");
+            }
+            System.out.println("Try one more time!");
+        }
+    }
+
+    static class BusinessException extends RuntimeException {
+        public BusinessException(String message) {
+            super(message);
+        }
+    }
+}
