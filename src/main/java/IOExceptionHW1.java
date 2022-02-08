6590305b9f9262d2aee70fcb12c123d90b3dbd16
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-08 21:53:59 +0300

    IOExceptionHW1 - Task completed

--- src/main/java/IOExceptionHW1.java
+++ src/main/java/IOExceptionHW1.java
@@ -1,30 +1,16 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class IOExceptionHW1 {
     public static void main(String[] args) throws Exception {
-        File file = new File("D:\\Test.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);    // запись в файл1
-        FileInputStream fileInputStream = new FileInputStream(file);       // чтение файл1
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
-                "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-                "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
-                "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
-                "mollit anim id est laborum.";
-        fileOutputStream.write(text.getBytes());
-        fileOutputStream.close();
-
-
-        File file2 = new File("D:\\CopyOfTest.txt");
-        FileOutputStream fileOutputStream2 = new FileOutputStream(file2);  // запись в файл2
-        int i = fileInputStream.read();                                    // чтение файл1
+        FileInputStream file1Reader = new FileInputStream("D:\\HW1_file1.txt");         // чтение файл1
+        FileOutputStream file2Writer= new FileOutputStream("D:\\HW1_file2.txt");        // запись в файл2
+        int i = file1Reader.read();
         while(i!= -1){
-            fileOutputStream2.write(i);
-            i = fileInputStream.read();
+            file2Writer.write(i);
+            i = file1Reader.read();
         }
     }
 }
-
 
 // 1) создать два файла. один заполнить данными (текст). второй должен скопировтаь данные из первого
