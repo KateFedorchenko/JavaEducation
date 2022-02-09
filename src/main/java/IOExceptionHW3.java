b47c5f86e38a671f19bec64849c32f66c5911b58
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-09 21:44:58 +0300

    IOExceptionHW3 - Task completed

--- src/main/java/IOExceptionHW3.java
+++ src/main/java/IOExceptionHW3.java
@@ -1,58 +1,36 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.BindException;
-import java.util.Arrays;
+import java.io.*;
 
 public class IOExceptionHW3 {
     public static void main(String[] args) throws IOException {
-        int numOfBytes = 0;
+        File reader = new File("D:\\HW3_file2.txt");
 
-        try (FileInputStream file1Reader = new FileInputStream("D:\\HW3_file1.txt");         // чтение файл1
-            FileOutputStream file2Writer = new FileOutputStream("D:\\HW3_file2.txt"))
-        {
-            int i = file1Reader.read();
-            while (i != -1) {
-                numOfBytes++;
-                i = file1Reader.read();
-            }
-            if(numOfBytes == 0) {
-                throw new BusinessException("The file is empty");
-            }
+        long size = countFileSize(reader);
+        byte[] arr = copyBytesFromFile(reader,(int) size);
 
-            int s = file1Reader.read();
-            int[] buffer = new int[numOfBytes];
-            for (int j = 0; j < buffer.length; j++) {
-                buffer[j] = s;
-                s = file1Reader.read();
+        try(FileOutputStream writer = new FileOutputStream("D:\\HW3_file1.txt")) {
+            for (int i = arr.length -1; i >= 0; i--) {
+                writer.write(arr[i]);
             }
-
-            for (int k = buffer.length-1; k >= 0; k--) {
-                int symbol = buffer[k];
-                file2Writer.write(symbol);
-            }
-            System.out.println(Arrays.toString(buffer));
-        }  catch (FileNotFoundException e) {
-            throw new BusinessException("No file has not been found");
         }
     }
 
-    static class BusinessException extends RuntimeException {
-        public BusinessException(String message) {
-            super(message);
+    public static long countFileSize(File name) throws IOException {
+        long numOfBytes = 0;
+        try (FileInputStream file = new FileInputStream(name)) {
+            int i = file.read();
+            while (i != -1) {
+                numOfBytes++;
+                i = file.read();
+            }
+            return numOfBytes;
+        }
+    }
+
+    public static byte[] copyBytesFromFile(File name, int numOfBytes) throws IOException {
+        try (FileInputStream file = new FileInputStream(name)) {
+            byte[] buffer = new byte[numOfBytes];
+            file.read(buffer);
+            return buffer;
         }
     }
 }
-
-
-/* Questions:
-1) Не до конца понимаю, как работает итератор символов в цикле while. Почему происходит перебор символов, если я не указываю
-это?
-2) Новая переменная s начинается не с начала файла, а присваивает последнее присвоенное значение i (= -1). Почему?
-
-Мой план:
-1) узнать кол-во символов
-2) в массив скопировать эти символы
-3) в обратном значения перекопировать символы в другой файл
-*/
