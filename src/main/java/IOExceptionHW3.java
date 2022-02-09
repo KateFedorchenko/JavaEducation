72f92b71849d9378d19214d2106da25c5edfa806
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-09 19:33:48 +0300

    IOExceptionHW3 - Task not completed - v.2

--- src/main/java/IOExceptionHW3.java
+++ src/main/java/IOExceptionHW3.java
@@ -1,26 +1,47 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.BindException;
 import java.util.Arrays;
 
 public class IOExceptionHW3 {
     public static void main(String[] args) throws IOException {
-        FileInputStream file1Reader = new FileInputStream("D:\\HW3_file1.txt");         // чтение файл1
-        FileOutputStream file2Writer= new FileOutputStream("D:\\HW3_file2.txt");        // запись в файл2
-
         int numOfBytes = 0;
-        int i = file1Reader.read();
-        while(i!= -1){
-            numOfBytes++;
-            i = file1Reader.read();
+        try (FileInputStream file1Reader = new FileInputStream("D:\\HW3_file1.txt");         // чтение файл1
+            FileOutputStream file2Writer = new FileOutputStream("D:\\HW3_file2.txt"))
+        {
+            int i = file1Reader.read();
+            while (i != -1) {
+                numOfBytes++;
+                i = file1Reader.read();
+            }
+           // file1Reader.close();
+            if(numOfBytes == 0) {
+                throw new BusinessException("The file is empty");
+            }
+
+            int s = file1Reader.read();
+            int[] buffer = new int[numOfBytes];
+            for (int j = 0; j < buffer.length; j++) {
+                buffer[j] = s;
+                s = file1Reader.read();
+            }
+
+            for (int k = buffer.length-1; k >= 0; k--) {
+                int symbol = buffer[k];
+                file2Writer.write(symbol);
+            }
+            System.out.println(Arrays.toString(buffer));
+        }  catch (FileNotFoundException e) {
+            throw new BusinessException("No file has not been found");
         }
 
-        int s = file1Reader.read(); // why the val is -1?
+    }
 
-        int[] buffer = new int[numOfBytes];
-        for (int j = 0; j < buffer.length; j++) {
-            buffer[j] = s;
-            s = file1Reader.read();
+    static class BusinessException extends RuntimeException {
+        public BusinessException(String message) {
+            super(message);
         }
     }
 }
