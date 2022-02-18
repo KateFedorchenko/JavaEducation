a3b6c9caaeb0b38fed184a3bd468172cb55270b8
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-19 00:58:33 +0300

    KthLargestNumberHW2 completed

--- src/main/java/collections/problem/KthLargestNumber.java
+++ src/main/java/collections/problem/KthLargestNumber.java
@@ -9,7 +9,6 @@ public class KthLargestNumber {
         List<Double> nums = new ArrayList<>(List.of(12.23121,20.421434,30.912421,3.3,4.3241124,54.3,55.24124215,489.2,4.5,4.6,4.7,4.2,3.2,4.3,4.9,4.8,55.2,53.2,755.2,342.2,53.2));
         System.out.println(nums);
         System.out.println(findKthLargestElement(nums,10));
-        System.out.println(getMaxValue(nums,10));
     }
     /**
      * Find the kTh largest element in the list.
@@ -24,20 +23,13 @@ public class KthLargestNumber {
         if(nums.size() < k) {
             throw new IllegalArgumentException("Too small to find largest element in the list");
         }
-        //possible solution but it performance poorly when size of nums much bigger than k.
-        List<Double> copy = new ArrayList<>(nums);
-        copy.sort(Comparator.naturalOrder());      // it changes the order --> breaks the rule
-        System.out.println(copy);
-        return copy.get(k-1);
-    }
-
-    static double getMaxValue(List<Double> arr, int k) {
-        double maxValue = arr.get(0);
+        double maxValue = nums.get(0);
         for(int i = 1; i < k; i++) {
-            if(maxValue < arr.get(i)) {
-                maxValue = arr.get(i);
+            if(maxValue < nums.get(i)) {
+                maxValue = nums.get(i);
             }
         }
         return maxValue;
     }
+
 }
