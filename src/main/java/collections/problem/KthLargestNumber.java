b598a75cc106b6ce0f93f7c7542cfe6a6e870cb2
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-19 00:25:41 +0300

    KthLargestNumberHW2 completed

--- src/main/java/collections/problem/KthLargestNumber.java
+++ src/main/java/collections/problem/KthLargestNumber.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 import java.util.List;
 
 public class KthLargestNumber {
+    public static void main(String[] args) {
+        List<Double> nums = new ArrayList<>(List.of(2.1,200.34,1.9,3.3,4.3,54.3,55.5));
+        System.out.println(nums);
+        System.out.println(findKthLargestElement(nums,3));
+    }
     /**
      * Find the kTh largest element in the list.
      * It is expected that nums list won't be modified in process.
@@ -15,13 +20,14 @@ public class KthLargestNumber {
      * @return KTh largest element in the list.
      */
     public static double findKthLargestElement(List<Double> nums, int k) {
+        if(nums.size() < k) {
+            throw new IllegalArgumentException("Too small to find largest element in the list");
+        }
         //possible solution but it performance poorly when size of nums much bigger than k.
         List<Double> copy = new ArrayList<>(nums);
-
         copy.sort(Comparator.naturalOrder());
-
-        return copy.get(k-1);
+        return copy.get(copy.size()-1);
     }
-
-
 }
+
+// not really got the idea of "when size of nums much bigger than k" but now it returns the correct value
