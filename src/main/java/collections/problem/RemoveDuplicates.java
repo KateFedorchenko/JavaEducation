d7309d6e68a2dc440d2a3df7db3fe3fdd0670d68
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-02-19 00:03:04 +0300

    RemoveDuplicatesHW1 completed with some questions

--- src/main/java/collections/problem/RemoveDuplicates.java
+++ src/main/java/collections/problem/RemoveDuplicates.java
@@ -1,13 +1,53 @@
 package collections.problem;
 
-import java.util.List;
+import java.util.*;
 
 public class RemoveDuplicates {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1,3,55,125,100,4,20,1,4));   // OR Arrays.asList(); - any difference? // another syntax -->  List<Integer> integers = new List<>(List.of((1,2,3,4,5,1,2,3)) does not work;
+        System.out.println(nums);
+        Collections.sort(nums);
+        //insertionSorting(nums);
+        //removeDuplicates(nums);
+        System.out.println(nums);
+    }
     /**
      * Remove duplicates values from given sorted array in-place.
      * @param nums Sorted array.
      */
     public static void removeDuplicates(List<Integer> nums) {
-
+        if(nums == null || nums.size() == 0) {
+            System.out.println("No list has been found");
+        }
+        for (int i = 0; i < nums.size()-1; i++) {
+            int valueForI = nums.get(i);
+            for (int j = i+1; j < nums.size(); j++) {
+                int valueForJ = nums.get(j);
+                if(valueForI == valueForJ) {
+                    nums.remove(j);
+                }
+            }
+        }
     }
+
+    /*static List<Integer> insertionSorting(List<Integer> arr) {
+        int currentElement;
+        int index;
+        for(int i = 0; i<arr.size(); i++) {
+            currentElement = arr.get(i);
+            index = i;
+            while(i > 0 && arr.get(i-1) > currentElement) {
+                arr.get(i) = arr.get(i-1);                    // arr.get(i) should be a variable. How to make it so within ArrayList scope?
+                index--;
+            }
+            arr.get(i) = currentElement;
+        }
+        return arr;
+    */
 }
+
+
+//Questions:
+// 1) I did smth that published all methods of List inside the  List<Integer> integers = new List<Integer>() {........}
+// 2) Differences among several initializations  ArrayList<>(List.of()); -- Arrays.asList(); -- .....
+// 3) How to use my sorting method for List<Integer> scope if there are problems with arr.get(i) -- which is not a variable and cannot be made so.
