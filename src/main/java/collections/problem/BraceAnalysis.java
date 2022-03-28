1356507a1ff5120642b0b4c10dd907fecb750925
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-03-28 19:55:38 +0300

    BraceAnalysis HW completed

--- src/main/java/collections/problem/BraceAnalysis.java
+++ src/main/java/collections/problem/BraceAnalysis.java
@@ -5,17 +5,24 @@ import java.util.Deque;
 
 public class BraceAnalysis {
     public static void main(String[] args) {
-        String str = "(())";
-        System.out.println(isCorrect(str));
-        String str2 = "(()(()))";
-        System.out.println(isCorrect(str2));
-        String str3 = "(";
-        System.out.println(isCorrect(str3));
-        String str4 = ")";
-        System.out.println(isCorrect(str4));
+        String str = "({}[ab]2f{})";
+        System.out.println(isCorrectModification2(str));
 
 
+//        String str = "(())";
+//        System.out.println(isCorrect(str));
+//        String str2 = "(()(()))";
+//        System.out.println(isCorrect(str2));
+//        String str3 = "())";
+//        System.out.println(isCorrect(str3));
+//        String str4 = ")";
+//        System.out.println(isCorrect(str4));
 
+//        System.out.println(isCorrectModification1(str));
+//        String str2 = "({}{}{}[])";
+//        System.out.println(isCorrectModification1(str2));
+//        String str3 = "({}{}{}[])[)";
+//        System.out.println(isCorrectModification1(str3));
     }
 
     /**
@@ -40,6 +47,108 @@ public class BraceAnalysis {
             }
         }
         return stack.isEmpty();
+    }
+
+    /**
+     * The function analyzes str which contains three types of braces (),[],{} and returns the result true if sequence is correct.
+     * For example, "(())" is correct, "(" is wrong, "(()(()))" is correct.
+     */
+    public static boolean isCorrectModification1(String str){
+        char[] arr = str.toCharArray();
+        Deque<Character> stackForParentheses = new ArrayDeque<>();
+        Deque<Character> stackForCurlyBraces = new ArrayDeque<>();
+        Deque<Character> stackForSquareBraces = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == '('){
+
+                stackForParentheses.push(arr[i]);
+            }
+            if(arr[i] == '{'){
+                stackForCurlyBraces.push(arr[i]);
+            }
+            if(arr[i] == '['){
+                stackForSquareBraces.push(arr[i]);
+            }
+
+
+            if(arr[i] == ')'){
+                if(stackForParentheses.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForParentheses.pop();
+                if(poppedChar != '('){
+                    return false;
+                }
+            }
+            if(arr[i] == '}'){
+                if(stackForCurlyBraces.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForCurlyBraces.pop();
+                if(poppedChar != '{'){
+                    return false;
+                }
+            }
+            if(arr[i] == ']'){
+                if(stackForSquareBraces.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForSquareBraces.pop();
+                if(poppedChar != '['){
+                    return false;
+                }
+            }
+        }
+        return stackForParentheses.isEmpty() && stackForCurlyBraces.isEmpty() && stackForSquareBraces.isEmpty();
+    }
+
+    public static boolean isCorrectModification2(String str){
+        char[] arr = str.toCharArray();
+        Deque<Character> stackForParentheses = new ArrayDeque<>();
+        Deque<Character> stackForCurlyBraces = new ArrayDeque<>();
+        Deque<Character> stackForSquareBraces = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == '('){
+
+                stackForParentheses.push(arr[i]);
+            }
+            if(arr[i] == '{'){
+                stackForCurlyBraces.push(arr[i]);
+            }
+            if(arr[i] == '['){
+                stackForSquareBraces.push(arr[i]);
+            }
+
+
+            if(arr[i] == ')'){
+                if(stackForParentheses.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForParentheses.pop();
+                if(poppedChar != '('){
+                    return false;
+                }
+            }
+            if(arr[i] == '}'){
+                if(stackForCurlyBraces.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForCurlyBraces.pop();
+                if(poppedChar != '{'){
+                    return false;
+                }
+            }
+            if(arr[i] == ']'){
+                if(stackForSquareBraces.isEmpty()){
+                    return false;
+                }
+                char poppedChar = stackForSquareBraces.pop();
+                if(poppedChar != '['){
+                    return false;
+                }
+            }
+        }
+        return stackForParentheses.isEmpty() && stackForCurlyBraces.isEmpty() && stackForSquareBraces.isEmpty();
     }
 }
 
