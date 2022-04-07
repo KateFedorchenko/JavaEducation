ba7f992de5499676c13fcf3206d4029f6248aa3c
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-07 21:17:21 +0300

    Decryptor HW - cannot cast Object Array to a String

---
+++ src/main/java/collections/problem/Decriptor.java
@@ -0,0 +1,44 @@
+package collections.problem;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * При инициализации декриптора передается таблица дешифрования - map с шифром. Map имеет тип : Map<Character,Character>.
+ * Реализовать метод string decript(String str){}. Примеры для теста. Сделать тесты для метода.
+ * return расшифрованный текст.
+ */
+public class Decriptor {
+    public static void main(String[] args) throws Exception {
+        decrypt("kbwb");
+    }
+
+    public static String decrypt(String str) throws Exception{
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i <= 25; i++) {
+            map.put((char) ('b' + i), (char) ('a' + i));
+        }
+
+        char[] chars = new char[str.length()];           // String to be converted to char array
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = str.charAt(i);
+        }
+
+        for (int i = 0; i < chars.length; i++) {        // check if there is any char that cannot be decrypted
+            if(map.containsKey(chars[i])){
+            } else {
+                throw new Exception("Your string cannot be decrypted - no such character for key has been found");
+            }
+        }
+
+        Object[] objectArray = new Object[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            objectArray[i] = map.get(chars[i]);
+        }
+
+        return Arrays.toString(objectArray);         // toString(Obj[] o) should convert my Object array to a String --> but it does not- why?
+    }
+
+
+}
