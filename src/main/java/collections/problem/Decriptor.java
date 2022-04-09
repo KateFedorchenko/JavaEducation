440a08c2d7f55de548991951984bba7c15ef1f14
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-09 14:27:53 +0300

    Decryptor HW fixed version

--- src/main/java/collections/problem/Decriptor.java
+++ src/main/java/collections/problem/Decriptor.java
@@ -11,6 +11,12 @@ import java.util.Map;
  * return расшифрованный текст.
  */
 public class Decriptor {
+    private Map<Character,Character> dictionary;       // called objective approach -- better -- too many 'static' word is bad
+
+    public Decriptor(Map<Character, Character> dictionary) {
+        this.dictionary = dictionary;
+    }
+
     public static void main(String[] args){
         Map<Character, Character> map = new HashMap<>();
         for (int i = 0; i < 25; i++) {
@@ -23,25 +29,23 @@ public class Decriptor {
         map.put('.', '.');
         map.put(',', ',');
         String str = "jo dpnqvujoh, b ibti ubcmf jt b ebub tusvduvsf uibu jnqmfnfout bo bttpdjbujwf bssbz bctusbdu ebub uzqf, b tusvduvsf uibu dbo nbq lfzt up wbmvft.";
-        System.out.println(decrypt(str, map));
+
+        Decriptor decriptor = new Decriptor(map);
+        System.out.println(decriptor.decrypt(str));
     }
 
-    public static String decrypt(String str, Map<Character,Character> map){
-        char[] chars = new char[str.length()];               // String to be converted to char array
-        for (int i = 0; i < str.length(); i++) {
-            chars[i] = str.charAt(i);
-        }
-
-        for (int i = 0; i < chars.length; i++) {            // check if there is any char that cannot be decrypted
-            if (!map.containsKey(chars[i])) {
+    public String decrypt(String str){
+        char[] chars = str.toCharArray();                     // standard method -- toCharArray() -- better to use
+        for(int i = 0; i < chars.length; i++) {
+            if (!dictionary.containsKey(chars[i])) {
                 throw new RuntimeException("Your string cannot be decrypted - no such character for key has been found");
             }
         }
         char[] objectArray = new char[chars.length];
         for (int i = 0; i < chars.length; i++) {
-            objectArray[i] = map.get(chars[i]);
+            objectArray[i] = dictionary.get(chars[i]);
         }
-        return new String(objectArray);                        // toString(Obj[] o) should convert my Object array to a String --> but it does not- why?
+        return new String(objectArray);
     }
 
 }
