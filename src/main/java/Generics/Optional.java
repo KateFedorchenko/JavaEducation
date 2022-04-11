3afa7a5a73a44f9981896f28becd5f241aff54ae
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-11 23:03:50 +0300

    Optional

---
+++ src/main/java/Generics/Optional.java
@@ -0,0 +1,22 @@
+package Generics;
+
+import java.util.NoSuchElementException;
+
+public class Optional<T> {
+    private T val;
+
+    public Optional(T val) {
+        this.val = val;
+    }
+
+    public T get(){
+        if(val == null) {
+            throw new NoSuchElementException();
+        }
+        return val;
+    }
+
+    public boolean isEmpty(){       // secure method -- no exceptions
+        return val == null;
+    }
+}
