5b16cdfc849c6a9163f1208cd6cd642af2debfe5
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-14 23:17:28 +0300

    ForwardList

--- src/main/java/generics/ForwardList.java
+++ src/main/java/generics/ForwardList.java
@@ -1,35 +1,35 @@
-package generics;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
-
-/**
- * All method should have time complexity of O(1).
- * @param <T>
- */
-public class ForwardList<T> /*implements Iterable<T>*/ {
-    /**
-     * @return  first element of the list.
-     */
-    public T front();
-
-    /**
-     *
-     * @return size of the list.
-     */
-    public T size();
-
-    /**
-     * Removes element from head.
-     * If list is already empty than throws {@link NoSuchElementException}.
-     */
-    public void removeFront();
-
-    /**
-     * Places new element to the front of the list.
-     */
-    public void pushFront(T newElement);
+//package generics;
+//
+//import java.util.Iterator;
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//
+///**
+// * All method should have time complexity of O(1).
+// * @param <T>
+// */
+//public class ForwardList<T> /*implements Iterable<T>*/ {
+//    /**
+//     * @return  first element of the list.
+//     */
+//    public T front();
+//
+//    /**
+//     *
+//     * @return size of the list.
+//     */
+//    public T size();
+//
+//    /**
+//     * Removes element from head.
+//     * If list is already empty than throws {@link NoSuchElementException}.
+//     */
+//    public void removeFront();
+//
+//    /**
+//     * Places new element to the front of the list.
+//     */
+//    public void pushFront(T newElement);
     /*
     @Override public Iterator<T> iterator() {
         return new ForwardListIterator<>();
@@ -45,4 +45,4 @@ public class ForwardList<T> /*implements Iterable<T>*/ {
             return null;
         }
     }  */
-}
+//}
