d06dad4cafec27272c47fc8b42a9d545f1c9cb76
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-14 23:17:06 +0300

    MajorityElementTest -- LeetCode Task1

--- src/main/java/LeetCode/MajorityElement.java
+++ src/main/java/LeetCode/MajorityElement.java
@@ -7,6 +7,9 @@ public class MajorityElement {
     }
 
     public static int findMajorityElement(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
         int count = 0;
         int majority = 0;
         for (int i = 0; i < nums.length-1; i++) {
