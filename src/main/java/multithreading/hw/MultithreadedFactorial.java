c8882c9c3c789851aa37ca46459f6c6ad9b86be2
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-04-22 23:33:36 +0300

    MultithreadedFactorial HW

--- src/main/java/multithreading/hw/MultithreadedFactorial.java
+++ src/main/java/multithreading/hw/MultithreadedFactorial.java
@@ -1,36 +1,50 @@
 package multithreading.hw;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MultithreadedFactorial {
+    public static void main(String[] args) throws Exception {
+        int factorial = 2;
+        int threadCount = 2;
+        int chunkSize = factorial / threadCount;
 
-    public static class MyThread extends Thread{
-        @Override
-        public void run() {
-            System.out.println("MyThread running");
+        List<MyFactorialThread> threads = new ArrayList<>();
+
+        for (int i = 0; i < threadCount; i++) {
+            MyFactorialThread t;
+            if (i == threadCount - 1) {
+                t = new MyFactorialThread(chunkSize*i,factorial+1); //!
+            } else {
+                t = new MyFactorialThread(chunkSize*i,chunkSize*(i+1)); //!
+            }
+            t.start();
+            threads.add(t);
         }
-    }
 
+        for (int i = 0; i < threadCount; i++) {
+            threads.get(i).join();
+        }
 
-    public static void main(String[] args) {
-        MyThread t = new MyThread();
-        t.start();
-        System.out.println(getFactorialMultithreaded(4,5));
+        long result = 0;
+        for (int i = 0; i < threadCount; i++) {
+            result += threads.get(i).getRes();          // hard to understand what it is for :/
+        }
+        System.out.println("result = " + result);
+        System.out.println("getFactorialMultithreaded() = " + getFactorialMultithreaded(factorial,threadCount));
     }
 
     /**
      * Calculates factorial e.g.
      * 10! = 10 * 9 * 8 *... * 1
-     * @param n means that factorial calculates of n (n! = n * (n-1) * (n-2) * ... * 1)
+     *
+     * @param n           means that factorial calculates of n (n! = n * (n-1) * (n-2) * ... * 1)
      * @param threadCount Number of thread need to be spawned for this task.
      * @return result of fact calculation.
      */
-    public static BigInteger getFactorialMultithreaded(int n, int threadCount) {
-        for (int i = 0; i < threadCount; i++) {
-            MyThread myThread = new MyThread();
-            myThread.start();
-        }
+    public static BigInteger getFactorialMultithreaded(int n, int threadCount) {    // why do I need here the param threadCount?
         if (n == 0) {
             return BigInteger.ZERO;
         }
@@ -42,5 +56,39 @@ public class MultithreadedFactorial {
             factorial = factorial.multiply(BigInteger.valueOf(i));
         }
         return factorial;
+    }
+
+
+    public static class MyFactorialThread extends Thread {
+        int from;
+        int to;
+
+        long res;
+
+        public MyFactorialThread(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public long getRes() {
+            return res;
+        }
+
+        @Override
+        public void run() {
+            for (int i = from; i < to; i++) {
+                res += i;
+            }
+        }
+    }
+
+    static class MyThread extends Thread {
+        public MyThread(){
+            super("MyAwesomeThread");
+        }
+        @Override
+        public void run(){
+            System.out.println("hello from another thread!");
+        }
     }
 }
