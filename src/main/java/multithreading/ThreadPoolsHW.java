e0794aea0d1a55bb5be098c177a13650d99aa846
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-05-17 23:48:10 +0300

    ThreadPoolsHW

---
+++ src/main/java/multithreading/ThreadPoolsHW.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+import java.math.BigDecimal;
+import java.util.concurrent.*;
+
+public class ThreadPoolsHW {
+    static ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable<BigDecimal> fib = new Callable<BigDecimal>() {
+            @Override
+            public BigDecimal call() throws Exception {
+                return fib(3);
+            }
+        };
+        Future<BigDecimal> fut = executorService.submit(fib);
+        System.out.println(fut.get());
+        executorService.shutdown();
+    }
+
+    static BigDecimal fib(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Please provide non-negative param");
+        }
+        if (x == 0) {
+            return BigDecimal.ZERO;
+        }
+        if (x == 1 || x == 2) {
+            return BigDecimal.ONE;
+        }
+        return fib(x - 1).add(fib(x - 2));
+    }
+}
