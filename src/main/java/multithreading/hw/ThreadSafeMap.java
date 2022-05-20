4131cdb1b3c477b2b05ffe5b5f15b9f08d504069
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-05-20 23:48:15 +0300

    ThreadSafeMap

--- src/main/java/multithreading/hw/ThreadSafeMap.java
+++ src/main/java/multithreading/hw/ThreadSafeMap.java
@@ -1,43 +1,71 @@
 package multithreading.hw;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class ThreadSafeMap{
-    int key;
-    String value;
+public class ThreadSafeMap<K, V> {
+    Map<K, V> threadSafeMap;
+    static ExecutorService executorService = Executors.newFixedThreadPool(4);
+    ReadWriteLock globalLock = new ReentrantReadWriteLock();
 
-
-    public ThreadSafeMap(int key, String value){
-        this.key = key;
-        this.value = value;
+    public ThreadSafeMap() {
+        threadSafeMap = new HashMap<>();
     }
 
+    public V put(K key, V value) {
+        globalLock.writeLock().lock();
+        try {
+            return threadSafeMap.put(key, value);
+        } finally {
+            globalLock.writeLock().unlock();
+        }
+    }
 
-//     put(){
-//
-//    }
-//
-//    get(){
-//
-//    }
-//
-//    size(){
-//
-//    }
-//
-//    replace(){
-//
-//    }
+    public V get(Object key) {
+        globalLock.readLock().lock();
+        try {
+            return threadSafeMap.get(key);
+        } finally {
+            globalLock.readLock().unlock();
+        }
+    }
 
-    public static void main(String[] args) {
-        ThreadSafeMap element1 = new ThreadSafeMap(1,"boo");
-        List<ThreadSafeMap> list = new ArrayList<>();
-        list.add(element1);
-        list.size();
+    public int size() {
+        globalLock.readLock().lock();
+        try {
+            return threadSafeMap.size();
+        } finally {
+            globalLock.readLock().unlock();
+        }
+    }
 
+    public V replace(K key, V value) {
+        globalLock.writeLock().lock();
+        try {
+            return threadSafeMap.replace(key, value);
+        } finally {
+            globalLock.writeLock().unlock();
+        }
+    }
 
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Runnable r = () -> {
+            ThreadSafeMap<Integer,String> map = new ThreadSafeMap<>();
+            map.put(0,"f");
+            map.put(1,"ff");
+            map.put(2,"fff");
+            map.put(3,"ffff");
+            System.out.println("map.get(0) = " + map.get(0));
+            System.out.println("map.size() = " + map.size());
+            System.out.println("map.replace(2,\"bbb\") = " + map.replace(2, "bbb"));
+            System.out.println("map.get(2) = " + map.get(2));
+        };
+        executorService.execute(r);
+        executorService.shutdown();
     }
 }
