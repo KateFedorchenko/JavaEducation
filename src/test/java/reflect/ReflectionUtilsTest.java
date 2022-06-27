038a4c5ce0ef0ea5b26bcf0eff6edc7f16b3e8a9
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-06-27 20:44:11 +0300

    ReflectionUtilsTest

--- src/test/java/reflect/ReflectionUtilsTest.java
+++ src/test/java/reflect/ReflectionUtilsTest.java
@@ -1,5 +1,6 @@
 package reflect;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -11,16 +12,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReflectionUtilsTest {
     @ParameterizedTest
-    @MethodSource("dataGenerator")      // actually value = "dataGenerator"
+    @MethodSource("dataGenerator")
+        // actually value = "dataGenerator"
     void test(Object lhs, Object rhs, boolean expected) {
         boolean actual = ReflectionUtils.equals(lhs, rhs);
         assertEquals(expected, actual);
     }
 
-    public static Stream<Arguments> dataGenerator() {
+    public static Stream<Arguments> dataGenerator() {           // 1) Is it ok to use here [in test] extra classes?
+        @AllArgsConstructor
+        class FirstData {
+            int x;
+            String str;
+        }
+
+        @AllArgsConstructor
+        class SecondData {
+            int x;
+            String str;
+        }
+
+        @AllArgsConstructor
+        class ThirdData {
+            String str;
+            int x;
+            int z;
+        }
+
+        @AllArgsConstructor
+        class FourthData {
+            int[] arr;
+            String str;
+        }
+
+        @AllArgsConstructor
+        class FifthData {
+            int[] arr;
+        }
+
         return Stream.of(
                 Arguments.of("foo", "foo", true),
-                Arguments.of("foo", "bar", false)
+                Arguments.of("foo", "bar", false),
+                Arguments.of(null, null, true),
+                Arguments.of(null, "foo", false),
+                Arguments.of("bar", null, false),
+                Arguments.of("12", 12, false),
+                Arguments.of("bar", 12, false),
+                Arguments.of(12, 12, true),
+                Arguments.of(12.01, 12, false),
+                Arguments.of(new int[]{1}, 1, false),
+                Arguments.of(new int[]{1,2,3}, new int[]{1,2,3}, true),
+                Arguments.of(new int[]{0}, new int[]{0,0}, false),
+                Arguments.of(new String[]{"foo"}, new String[]{"foo"}, true),
+                Arguments.of(new String[]{"foo","bar"}, new String[]{"bar","foo"}, false),
+                Arguments.of(new String[]{"a","b"}, new String[]{"a","b","c"}, false),
+                Arguments.of(new String[]{"a","b",null}, new String[]{"a","b","c"}, false),
+                Arguments.of(new String[]{null}, new String[]{null}, true),
+                Arguments.of(new FirstData(2, "basket"), new FirstData(2, "bloopers"), false),
+                Arguments.of(new SecondData(2, "basket"), new FirstData(2, "basket"), true),
+                Arguments.of(new ThirdData("basket", 2, 3), new FirstData(2, "basket"), false),
+                Arguments.of(new FourthData(new int[]{1,2,3}, "data"), new FourthData(new int[]{1,2,3}, "data"), true),
+                Arguments.of(new FourthData(new int[]{1,2,3}, "data"), new FifthData(new int[]{1,2,3}), false)
         );
     }
 }
