68aa99c1c4771c701169ba1c1e5e5c9e12b76b79
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-07-02 14:42:35 +0300

    ArrayCopyHW

--- src/main/java/reflect/hw/ArrayCopy.java
+++ src/main/java/reflect/hw/ArrayCopy.java
@@ -4,39 +4,25 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayCopy {
-    public static <T> T[] copy(T[] object) {        // how to make it for 2-dim. arrays?
-        /**
-         * Reflection approach, right?
-         */
+    public static <T> T[] copy(T[] object) {
+//        Class<?> claz = object.getClass();
         Class<?> sourceArrType = object.getClass().getComponentType();
         T[] copy = (T[]) Array.newInstance(sourceArrType, object.length);
+
+         //To replace with cycle.
+
         System.arraycopy(object, 0, copy, 0, object.length);
         return copy;
-
-        /**
-         * Standard approach, right?
-         */
-//        T[] newObject = Arrays.copyOf(object,object.length);
-//        return newObject;
-
-        /**
-         * Deep copy, right?
-         */
-        //return object.clone();
     }
 
     public static void main(String[] args) {
-        String[] str = {"foo"};
-        String[] newStr = copy(str);
-        System.out.println("Before -> str = " + Arrays.toString(str));
-        str[0] = "bar";
-        System.out.println("After -> str = " + Arrays.toString(str));
-        System.out.println("newStr = " + Arrays.toString(newStr));
+        Object o = new String("foo");
+        String o2 = (String) o;
 
-        int[] oldArr = {1,2};
-//        int[] newArr = (int[])copy(oldArr);       ????
-
-
+        String[][] arr = {{"foo"},{"bar"}};
+        String[][] arr2 = copy(arr);
+        arr[0][0] = "xyz";
+        System.out.println(arr2[0][0]);
     }
 }
 
